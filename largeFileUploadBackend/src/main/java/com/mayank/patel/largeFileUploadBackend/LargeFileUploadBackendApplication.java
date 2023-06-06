b19965103e5f115b39397f721190bc1cc2c17c81
mayhim182 <mayankatnitrkl@gmail.com>
2023-06-06 09:42:44 +0530

    adding backend structure

---
+++ largeFileUploadBackend/src/main/java/com/mayank/patel/largeFileUploadBackend/LargeFileUploadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.patel.largeFileUploadBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeFileUploadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeFileUploadBackendApplication.class, args);
+	}
+
+}
---
+++ largeFileUploadBackend/src/test/java/com/mayank/patel/largeFileUploadBackend/LargeFileUploadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.patel.largeFileUploadBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeFileUploadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
